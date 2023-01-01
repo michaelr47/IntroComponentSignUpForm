14e4900fef62942f4b20a8905d73639daba6df73
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2022-12-31 21:59:18 -0800

    left header and description side done

---
+++ app.js
@@ -0,0 +1,6 @@
+const fNameInput = document.getElementById('firstNameInput');
+const lNameInput = document.getElementById('lastNameInput');
+const emailInput = document.getElementById('emailInput');
+const passwordInput = document.getElementById('passwordInput');
+const submit = document.getElementById('claimButton');
+
