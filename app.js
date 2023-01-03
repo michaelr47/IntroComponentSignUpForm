824c8cf9df83a11031c531ff0284a7a3fd4505a6
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-01-02 20:49:47 -0800

    validation error and text displayed, onto functionality

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ const emailInput = document.getElementById('emailInput');
 const passwordInput = document.getElementById('passwordInput');
 const submit = document.getElementById('claimButton');
 
+// function checkIfValid() {
+
+    let spanInvalid = document.querySelectorAll('.invalidText');
+    spanInvalid.style.display = 'none';
+    const inputFeilds = document.getElementsByTagName("input");
+    const validInputs = Array.from(inputFeilds).filter( input => input.value !== "");
+    // if (validInputs) {
+    //     // return;
+    // } else {
+        
+    // }
+// }
